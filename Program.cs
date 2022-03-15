80a58fba08e2481d56726170da5134e5860387f3
Marklarionov <93324243+Marklarionov@users.noreply.github.com>
2022-03-15 19:54:04 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,44 +10,68 @@ namespace Independent_work
     {
         static void Main(string[] args)
         {
-            //1
+            //--1--
+
             /*Console.WriteLine("Enter a string of arbitrary length:");
             string str = Console.ReadLine();
             Console.WriteLine("Enter a character to find the percentage of its occurrence in a string:");
             char sum = Console.ReadKey().KeyChar;
             Console.WriteLine();
 
-            int length = str.Length;
-            char[] strArr = str.ToCharArray();
+            char[] tahed = str.ToCharArray();
             int counter = 0;
-            for (int i = 0; i < length; i++)
+            for (int i = 0; i < str.Length; i++)
             {
-                if (strArr[i] == sum)
+                if (tahed[i] == sum)
                 {
                     counter++;
                 }
             }
-            double Dlength = Convert.ToDouble(length);
-            double Dcounter = Convert.ToDouble(counter);
-            double percent = (Dcounter / Dlength) * 100;
-            Console.WriteLine($"Character occurrence percentage {sum} equals {percent}%");
-            Console.ReadLine();*/
+            double protsent = (Convert.ToDouble(counter) / Convert.ToDouble(str.Length)) * 100;
+            Console.WriteLine($"Character occurrence percentage {sum} equals  {Math.Round(protsent, 2)}%");*/
 
-            //2
-            int[,] mass = new int[10, 10];
+            //--2--
+
+            int[,] M = new int[10, 10];
             Random random = new Random();
-            for (int i = 0; i < mass.GetLength(0); i++)
+            for (int i = 0; i < M.GetLength(0); i++)
             {
-                for (int j = 0; j < mass.GetLength(1); j++)
+                for (int j = 0; j < M.GetLength(1); j++)
                 {
-                    mass[i, j] = random.Next(1, 20);
-                    Console.Write(mass[i, j] + " ");
+                    M[i, j] = random.Next(1, 5);
+                    Console.Write(M[i, j] + " ");
                 }
+                Console.WriteLine();
             }
-
-
-
-
+            int max = M[0, 0];
+            for (int i = 0; i < M.GetLength(0); i++)
+            {
+                double pr = 1;
+                double sum = 0;
+                for (int j = 0; j < M.GetLength(1); j++)
+                {
+                    pr = pr * M[j, i];
+                    sum = sum + M[i, j];
+                    if (i == j)
+                        if (max < M[i, j])
+                            max = M[i, j];
+                }
+                Console.WriteLine("multiplication " + (i + 1) + " column =" + pr);
+                Console.WriteLine("Sum " + (i + 1) + " string=" + sum);
+            }
+            Console.WriteLine("Max element " + max);
+            
+            //--3--
+            
+            /*string[,] inimene = new string[3, 2];
+            Console.WriteLine("Write your name,surname and group");
+            inimene[1, 1] = Console.ReadLine();
+            inimene[1, 1] = Console.ReadLine();
+            inimene[1, 1] = Console.ReadLine();
+            Console.WriteLine("Period estimate");
+            int a = int.Parse(Console.ReadLine());
+            int[] M = new int[a];
+            Console.WriteLine("Write your grades");*/
 
             Console.ReadLine();
         }
